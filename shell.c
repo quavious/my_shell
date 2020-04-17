66668d7eb9b90dbb8e1069c4d58d780f321996c1
quavious <leenowon97@gmail.com>
2020-04-17 14:26:05 +0900

    comment added

--- shell.c
+++ shell.c
@@ -10,17 +10,17 @@ void custom_signal(int signo){
 	printf("\nAGAIN : ");
 }
 
-char** get_env(void){
+char** get_env(void){ // 환경변수 설정 함수
 	char *ptr, *ch, *next_ptr;
-	char **tok = malloc(sizeof(char*) * 20);
+	char **tok = malloc(sizeof(char*) * 20); // 이중 포인터(포인터 배열) 생성
 	int i = 0;
 
-	ch = getenv("PATH");
-	for(i=0, ptr=ch; ;ptr=NULL,i++){
-		tok[i] = strtok_r(ptr, ":", &next_ptr);
-		if(tok[i] == NULL) break;
+	ch = getenv("PATH"); // PATH 환경변수 가져오기
+	for(i=0, ptr=ch; ;ptr=NULL,i++){ // strtok_r을 사용하기 위해 문자열 복사
+		tok[i] = strtok_r(ptr, ":", &next_ptr); //문자열 : 기준으로 잘라서 tok 배열에 보관
+		if(tok[i] == NULL) break; // 남아있는 문자열이 없으면 break
 	}
-	tok[i] = NULL;
+	tok[i] = NULL; //환경변수 배열 마지막에는 null이 있어야 함
 	return tok;
 }
 
@@ -46,12 +46,12 @@ int cd(char *path){
 }
 
 int main(void) {
-	char **e = get_env();
-	char *env[20];
+	char **e = get_env(); // 환경변수 가져오기
+	char *env[20]; //복사할 포인터 배열
 	pid_t pid;
 	int stat_loc;
 
-	signal(SIGINT, custom_signal);
+	signal(SIGINT, custom_signal); //컨트롤 C에 의해 종료되지 않도록 함
 
 	for(int i=0;i<20;i++){
 		if(e[i]==NULL){
@@ -61,24 +61,24 @@ int main(void) {
 		env[i] = e[i];
 	}
 
-	free(e);
+	free(e); //malloc으로 할당된 e 포인터 free
 
 	while(1) {
 		int i = 0;
 		char *nextptr, *parsed, *ptr;
-		char *command[20];
-		char input[20];
-		char keyword[30] = "/bin/";
-		char buf[255];
+		char *command[20]; //명령어 배열 보관
+		char input[20]; //입력된 명령어를 임시 보관할 배열
+		char keyword[30] = "/bin/"; //명령어는 bin 폴더에 보관되어 있음
+		char buf[255]; //현재 위치 받아올 문자열(배열)
 		
 		printf("custom shell | %s $>", getcwd(buf, 255));
-		fgets(input,19,stdin);
+		fgets(input,19,stdin); 
 		//input[strlen(input) - 1] = '\0';
 		
 		ptr = strchr(input, '\n');
-		if(ptr) *ptr=0;
+		if(ptr) *ptr=0; // 문자열 맨 끝 개행 문자 없애기
 
-		if(strcmp("exit",input) == 0) {
+		if(strcmp("exit",input) == 0) { //exit 명령어 뜨면 종료하도록
 			//free(input);
 			printf("Exiting...\n");
 			break;
@@ -93,7 +93,7 @@ int main(void) {
 		}
 		command[i] = NULL;
 
-		if(strcmp("cd", command[0]) == 0) {
+		if(strcmp("cd", command[0]) == 0) { // cd 명령어를 입력받으면 따로 수행하도록
 			if(cd(command[1]) < 0) {
 				perror(command[1]);
 			}
@@ -107,7 +107,7 @@ int main(void) {
 			exit(1);
 		}
 		if(pid == 0){
-			if(execve(keyword, command, env) == -1) {
+			if(execve(keyword, command, env) == -1) { //keyword에는 명령어 파일의 전체 경로를 명시해야 함
 				perror("FAILED");
 				exit(0);
 			}
